bccd5c4f8c58932846334df8d360462247356987
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2017-04-18 17:11:03 -0400

    Prepare for initial Heroku push

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -6,8 +6,10 @@ class Api::SessionsController < ApplicationController
   def show
   end
 
-  def destroy
-    @user = current_user
-    if @user
-  end
+  # def destroy
+  #   @user = current_user
+  #   if @user
+  #     logout
+  #     render "api/users/show"
+  # end
 end
