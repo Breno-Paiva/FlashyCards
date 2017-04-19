04ff979899ae6d2b9ff1127d1e2c9535a693d071
Breno_Paiva <breno.paiva93@gmail.com>
2017-04-19 10:44:14 -0400

    Update gemfile so Signup and Login persist to DB

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -6,12 +6,29 @@ import App from './app';
 import SessionFormContainer from './session_form/session_form_container';
 
 const Root = ({ store }) => {
+
+  const _ensureLoggedIn = (nextState, replace) => {
+  const currentUser = store.getState().session.currentUser;
+  if (!currentUser) {
+    replace('/login');
+  }
+};
+
+const _redirectIfLoggedIn = (nextState, replace) => {
+  const currentUser = store.getState().session.currentUser;
+  if (currentUser) {
+    replace('/');
+  }
+}
+
+
+
   return (
     <Provider store={store}>
       <Router history={hashHistory}>
         <Route path="/" component={App}>
-          <Route path="signup" component={SessionFormContainer} />
-          <Route path="login" component={SessionFormContainer} />
+          <Route path="signup" component={SessionFormContainer} onEnter={_redirectIfLoggedIn} />
+          <Route path="login" component={SessionFormContainer} onEnter={_redirectIfLoggedIn} />
         </Route>
       </Router>
     </Provider>
