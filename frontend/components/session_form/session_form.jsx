b4260007ea4e4bf8ec6bfb7bd2f5ffd4d325e7da
Breno_Paiva <breno.paiva93@gmail.com>
2017-04-19 09:29:43 -0400

    Complete session_form container auth

--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -6,26 +6,62 @@ class SessionForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = { username: "", password: "" };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componenetDidUpdate() {
+    this.redirectIfLoggedIn();
+  }
+
+  redirectIfLoggedIn() {
+    if (this.props.loggedIn) {
+      this.props.router.push('/');
+    }
   }
 
   update(field) {
     return e => this.setState({ [field]: e.currentTarget.value });
   }
 
-  render() {
-    return(
-      <form>
-        <label>username:
-          <input onChange={this.update("username")} />
-        </label>
-        <br/>
-        <label>password:
-          <input onChange={this.update("password")} />
-        </label>
-      </form>
-    );
+  handleSubmit(e) {
+    debugger
+    e.preventDefault();
+    const user = this.state;
+    this.props.processForm({user});
   }
+
+renderErrors() {
+  return (
+    <ul>
+      {this.props.errors.map((error, i) => (
+        <li key={`error-${i}`}>
+          {error}
+        </li>
+      ))}
+    </ul>
+  );
 }
 
-// withRouter
-export default SessionForm;
+  render() {
+    return(
+      <div>
+        {this.renderErrors()}        
+        <form onSubmit={this.handleSubmit}>
+          <h3>{this.props.formType}</h3>
+          <label>username:
+            <input type="text" value={this.state.username} onChange={this.update("username")} />
+          </label>
+          <br/>
+          <label>password:
+            <input type="password" value={this.state.password} onChange={this.update("password")} />
+          </label>
+          <br/>
+          <input type="submit" value={`${this.props.formType}!`} />
+        </form>
+      </div>
+    );
+  }
+
+}
+
+export default withRouter(SessionForm);
