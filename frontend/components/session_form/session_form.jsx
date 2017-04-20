6f9287c5d397e432ee758a07972e5108f821adf6
Breno Paiva <breno.paiva93@gmail.com>
2017-04-20 09:44:50 -0400

    Edit session form to only show on page while active

--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -32,7 +32,7 @@ class SessionForm extends React.Component {
 
   renderErrors() {
     return (
-      <ul>
+      <ul className="session-form-errors">
         {this.props.errors.map((error, i) => (
           <li key={`error-${i}`}>
             {error}
@@ -52,27 +52,25 @@ class SessionForm extends React.Component {
 
   render() {
     return(
-      <div className={`session-form-container ${this.props.className}`}>
-        <form onSubmit={this.handleSubmit}>
-          <h4>{this.props.formType}</h4>
-          {this.renderErrors()}
-          <input placeholder="username"
-            type="text"
-            value={this.state.username}
-            onChange={this.update("username")}
-          />
-          <br/>
-          <input placeholder="password"
-            type="password"
-            value={this.state.password}
-            onChange={this.update("password")}
-          />
-          <br/>
-          <input  type="submit" value={`${this.props.formType}!`} />
-          <br/>
-          {this.navLink()}
-        </form>
-      </div>
+      <form onSubmit={this.handleSubmit} className={`session-form-container ${this.props.className}`}>
+        <h4>{this.props.formType}</h4>
+        {this.renderErrors()}
+        <input placeholder="username"
+          type="text"
+          value={this.state.username}
+          onChange={this.update("username")}
+        />
+        <br/>
+        <input placeholder="password"
+          type="password"
+          value={this.state.password}
+          onChange={this.update("password")}
+        />
+        <br/>
+        <input  type="submit" value={`${this.props.formType}!`} />
+        <br/>
+        {this.navLink()}
+      </form>
     );
   }
 
