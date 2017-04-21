b2d5cb7a17e890bf13e703e388bade84aa301f01
Breno_Paiva <breno.paiva93@gmail.com>
2017-04-21 19:46:38 -0400

    Remove subject item component and render all subject in a list

--- frontend/components/subject/subject.jsx
+++ frontend/components/subject/subject.jsx
@@ -3,12 +3,28 @@ import { Link } from 'react-router';
 
 class Subject extends React.Component {
 
+  componentDidMount(){
+    this.props.fetchSubjects();
+  }
+
   // this.props.fetchSubjects()
   render() {
     return(
-      <h2>subject index ul</h2>
+      <div>
+        <h2>subject index ul</h2>
+        <ul>
+          {
+            this.props.subjects.map(subject  =>  {
+              return (
+                <li>
+                  {subject.name}
+                </li>
+              )})
+          }
+        </ul>
+      </div>
     )
   }
-}
+};
 
 export default Subject;
