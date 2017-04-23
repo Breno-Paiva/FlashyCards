0be158619531465b99ada0a32ed46053b774e128
Breno Paiva <breno.paiva93@gmail.com>
2017-04-23 13:13:18 -0400

    Render deck component

--- frontend/components/library/library.jsx
+++ frontend/components/library/library.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link, withRouter } from 'react-router';
 import Subject from '../subject/subject_container';
+import Deck from '../deck/deck_container';
 
 class Library extends React.Component {
 
@@ -11,6 +12,7 @@ class Library extends React.Component {
       <div className="library-container">
         <h3>dis be da library page</h3>
         <Subject />
+        <Deck />
       </div>
     );
   }
