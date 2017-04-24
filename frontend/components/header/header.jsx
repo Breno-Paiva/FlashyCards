b7df69b091fabb2a16c2d5b56045b32c374ebfe1
Breno_Paiva <breno.paiva93@gmail.com>
2017-04-23 23:34:10 -0400

    Add favicon and Civics seeds

--- frontend/components/header/header.jsx
+++ frontend/components/header/header.jsx
@@ -54,6 +54,8 @@ class Header extends React.Component {
     }
   }
 
+  // Business graphic by <a href="http://www.flaticon.com/authors/freepik">Freepik</a> from <a href="http://www.flaticon.com/">Flaticon</a> is licensed under <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0">CC BY 3.0</a>. Made with <a href="http://logomakr.com" title="Logo Maker">Logo Maker</a>
+  // <img src={require('../../../app/assets/images/Logomakr_8yalAw.png')}></img>
   leftHeader () {
     return (
       <div className="left-header">
