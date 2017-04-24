847cb6b9e1dbf4e8f198c702b782d7e0375f4c76
Breno Paiva <breno.paiva93@gmail.com>
2017-04-24 17:59:56 -0400

    Create card table and model

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -2,5 +2,6 @@ class Deck < ApplicationRecord
   validates :name, presence: true
 
   belongs_to :subject
+  has_many :cards
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170422191327) do
+ActiveRecord::Schema.define(version: 20170424215415) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cards", force: :cascade do |t|
+    t.string   "question",   null: false
+    t.string   "answer",     null: false
+    t.integer  "deck_id",    null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "decks", force: :cascade do |t|
     t.string   "name",       null: false
