95c2d8c8d0410ba8b7558c1875512f9e220a9ce0
Breno Paiva <breno.paiva93@gmail.com>
2017-04-25 00:55:18 -0400

    Add css styling for card list items using flex

--- frontend/components/card/card.jsx
+++ frontend/components/card/card.jsx
@@ -15,18 +15,39 @@ class Card extends React.Component {
             <h2>{`these are cards for deck #${this.props.params.deckId} `}</h2>
             <ul>
               <li>#</li>
-              <li>question</li>
-              <li>answer</li>
+              <li id="question-list">question</li>
+              <li id="answer-list">answer</li>
+              <li>x</li>
             </ul>
         </div>
 
         <div className="card-list">
           <ol>
-            <li>first</li>
-            <li>second</li>
-            <li>third</li>
-            <li>fourth</li>
-            <li>fifth</li>
+            <li>
+              <h4 id="question-item">first question</h4>
+              <h4 id="answer-item">first answer</h4>
+              <h4 id="x-card-item">x</h4>
+            </li>
+            <li>
+              <h4 id="question-item">second question</h4>
+              <h4 id="answer-item">second answer</h4>
+              <h4 id="x-card-item">x</h4>
+            </li>
+            <li>
+              <h4 id="question-item">third question</h4>
+              <h4 id="answer-item">third answer</h4>
+              <h4 id="x-card-item">x</h4>
+            </li>
+            <li>
+              <h4 id="question-item">fourth question</h4>
+              <h4 id="answer-item">fourth answer</h4>
+              <h4 id="x-card-item">x</h4>
+            </li>
+            <li>
+              <h4 id="question-item">fifth question</h4>
+              <h4 id="answer-item">fifth answer</h4>
+              <h4 id="x-card-item">x</h4>
+            </li>
           </ol>
         </div>
       </div>
