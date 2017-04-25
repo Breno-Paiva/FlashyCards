bb4d0f95b41a069c4178f8246cb99b647ff2483c
Breno Paiva <breno.paiva93@gmail.com>
2017-04-25 10:49:05 -0400

    Show existing card values in textarea

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import SessionReducer from './session_reducer';
 import SubjectReducer from './subject_reducer';
 import DeckReducer from './deck_reducer';
+import CardReducer from './card_reducer';
 
 const RootReducer = combineReducers({
   session: SessionReducer,
   subject: SubjectReducer,
-  deck: DeckReducer
+  deck: DeckReducer,
+  card: CardReducer
 });
 
 export default RootReducer;
