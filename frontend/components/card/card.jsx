94629cf2cf524c2d7976ed338f003eb89d77727e
Breno_Paiva <breno.paiva93@gmail.com>
2017-04-25 08:02:04 -0400

    Add textarea to cards form

--- frontend/components/card/card.jsx
+++ frontend/components/card/card.jsx
@@ -12,11 +12,11 @@ class Card extends React.Component {
     return (
       <div className="card-container">
         <div className="card-header">
-            <h2>{`these are cards for deck #${this.props.params.deckId} `}</h2>
+            <h2>{`These are cards for deck #${this.props.params.deckId} `}</h2>
             <ul>
               <li>#</li>
-              <li id="question-list">question</li>
-              <li id="answer-list">answer</li>
+              <li id="question-list">Question</li>
+              <li id="answer-list">Answer</li>
               <li>x</li>
             </ul>
         </div>
@@ -24,31 +24,41 @@ class Card extends React.Component {
         <div className="card-list">
           <ol>
             <li>
-              <h4 id="question-item">first question</h4>
-              <h4 id="answer-item">first answer</h4>
+              <textarea id="question-item">first question</textarea>
+              <textarea id="answer-item">first answer</textarea>
               <h4 id="x-card-item">x</h4>
             </li>
             <li>
-              <h4 id="question-item">second question</h4>
-              <h4 id="answer-item">second answer</h4>
+              <textarea id="question-item">first question</textarea>
+              <textarea id="answer-item">first answer</textarea>
               <h4 id="x-card-item">x</h4>
             </li>
             <li>
-              <h4 id="question-item">third question</h4>
-              <h4 id="answer-item">third answer</h4>
+              <textarea id="question-item">second question</textarea>
+              <textarea id="answer-item">second answer</textarea>
               <h4 id="x-card-item">x</h4>
             </li>
             <li>
-              <h4 id="question-item">fourth question</h4>
-              <h4 id="answer-item">fourth answer</h4>
+              <textarea id="question-item">third question</textarea>
+              <textarea id="answer-item">third answer</textarea>
               <h4 id="x-card-item">x</h4>
             </li>
             <li>
-              <h4 id="question-item">fifth question</h4>
-              <h4 id="answer-item">fifth answer</h4>
+              <textarea id="question-item">fourth question</textarea>
+              <textarea id="answer-item">fourth answer</textarea>
+              <h4 id="x-card-item">x</h4>
+            </li>
+            <li>
+              <textarea id="question-item">fifth question</textarea>
+              <textarea id="answer-item">fifth answer</textarea>
               <h4 id="x-card-item">x</h4>
             </li>
           </ol>
+          <div className="card-buttons group">
+            <button>Reset</button>
+            <button>Save this Deck</button>
+            <button>Start studying</button>
+          </div>
         </div>
       </div>
     )
