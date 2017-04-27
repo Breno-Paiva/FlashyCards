8c595e31906f08848cca18ae4f02201c9c79d9c7
Breno_Paiva <breno.paiva93@gmail.com>
2017-04-27 09:08:16 -0400

    initial styling in study container

---
+++ frontend/components/study/study_card.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link, hashHistory } from 'react-router';
+import update from 'immutability-helper';
+
+class StudyCard extends React.Component {
+
+  constructor(props){
+    super(props);
+  }
+
+  render () {
+
+    return (
+      <div className="study-card-container">
+      </div>
+    )
+  }
+}
+
+export default StudyCard;
