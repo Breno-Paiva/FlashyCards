2b20feabff25e2ce1926fa309016d95adf93d0c5
Breno_Paiva <breno.paiva93@gmail.com>
2017-05-08 14:35:12 -0400

    Add production readme pics

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -10,3 +10,14 @@ const configureStore = (preloadedState = {}) => (
 );
 
 export default configureStore;
+
+// const middlewares = [thunk];
+//
+// if (process.env.NODE_ENV === `development`) {
+//   const { logger } = require(`redux-logger`);
+//     middlewares.push(logger);
+//   }
+//
+// const configureStore = (preloadedState = {}) => (
+//   createStore(RootReducer, preloadedState, applyMiddleware(...middlewares))
+// );
