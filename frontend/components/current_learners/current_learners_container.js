86e56d10c7125447f2dfe15bc6f8c71e28934907
Breno Paiva <breno.paiva93@gmail.com>
2017-05-10 09:57:14 -0400

    Show random hints

--- frontend/components/current_learners/current_learners_container.js
+++ frontend/components/current_learners/current_learners_container.js
@@ -1,12 +1,24 @@
 import React from 'react';
-// import { Link, withRouter } from 'react-router';
+
+const HINTS = [
+  "A deck a day keeps the brain awake.",
+  "The more you study the better you'll be.",
+  "You can do it! Keep up the good work.",
+  "It's been proven that flash cards make you smarter.",
+  "Need to study a new subject? Just create some decks!"
+]
 
 class CurrentLearners extends React.Component {
+
+  randId(){
+    return Math.floor(Math.random()*HINTS.length);
+  }
 
   render() {
     return(
       <div className="current-learners-container">
-        {"'Current Learners'  feature coming soon..."}
+        <i className="fa fa-lightbulb-o fa-lg" aria-hidden="true"></i>
+        {HINTS[this.randId()]}
       </div>
     );
   }
