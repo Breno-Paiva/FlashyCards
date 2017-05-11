a1a9077ad31984ab7f2311463620db07e473277c
Breno Paiva <breno.paiva93@gmail.com>
2017-05-11 09:18:47 -0400

    Add and style modal for cog

--- frontend/components/deck/deck.jsx
+++ frontend/components/deck/deck.jsx
@@ -5,7 +5,7 @@ class Deck extends React.Component {
 
   constructor(props){
     super(props);
-    this.state = {name: "", subject_id: this.props.currentSubjectId};
+    this.state = {name: "", subject_id: this.props.currentSubjectId, modalIsOpen: false, modalId: null};
     this.createDeck = this.createDeck.bind(this);
     this.deleteDeck = this.deleteDeck.bind(this);
     this.currentSubject = this.currentSubject.bind(this);
@@ -13,6 +13,9 @@ class Deck extends React.Component {
     this.renderDeckButton = this.renderDeckButton.bind(this);
     this.addCardButton = this.addCardButton.bind(this);
     this.deleteSubject = this.deleteSubject.bind(this);
+    this.renderCogModal = this.renderCogModal.bind(this);
+    this.toggleCog = this.toggleCog.bind(this);
+    this.removeCog = this.removeCog.bind(this);
 
   }
 
@@ -81,23 +84,53 @@ class Deck extends React.Component {
 
   studyButton(deck){
     return (
-      <div className="manage-deck-buttons">
+      <div className="manage-deck-buttons" onMouseLeave={()=>this.removeCog()}>
         <Link to={`/study/${deck.id}`}>
           <div className="study-button">
             <i className="fa fa-play-circle-o" aria-hidden="true"></i>
             Study
           </div>
         </Link>
-        <div className="deck-settings">
+        <div className="deck-settings" onMouseOver={()=>this.toggleCog(deck)}>
           <i className="fa fa-cog" aria-hidden="true"></i>
         </div>
+        {this.renderCogModal(deck)}
       </div>
     )
   }
 
+  toggleCog(deck){
+    if (this.state.modalIsOpen){
+      this.setState({modalId: null})
+    }else{
+      this.setState({modalId: deck.id})
+    }
+  }
+
+  removeCog(){
+    this.setState({modalId: null})
+  }
+
+  renderCogModal(deck){
+    if (this.state.modalId === deck.id){
+      return <div className="deck-cog">
+              <ul>
+                <li>
+                  <Link to={`/decks/${deck.id}/cards`}>
+                    <i className="fa fa-pencil" aria-hidden="true"></i> edit cards
+                  </Link>
+                </li>
+                <li onClick={() => this.deleteDeck(deck)}>
+                  <i className="fa fa-minus-circle" aria-hidden="true"></i> delete
+                </li>
+              </ul>
+             </div>
+    }
+  }
+
   addCardButton(deck){
     return(
-      <div className="manage-add-buttons">
+      <div className="manage-add-buttons" onMouseLeave={()=>this.removeCog()}>
         <Link to={`/decks/${deck.id}/cards`}>
           <div className="add-card-button">
             <i className="fa fa-plus" aria-hidden="true"></i>
@@ -105,8 +138,9 @@ class Deck extends React.Component {
           </div>
         </Link>
         <div className="deck-settings">
-          <i className="fa fa-cog" aria-hidden="true"></i>
+          <i className="fa fa-cog" aria-hidden="true" onMouseOver={()=>this.toggleCog(deck)}></i>
         </div>
+        {this.renderCogModal(deck)}
       </div>
     )
   }
