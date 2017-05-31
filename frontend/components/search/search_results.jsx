07ce1e899b45cb2105f17a3ca0f2747c8d8af8fb
Breno Paiva <breno.paiva93@gmail.com>
2017-05-31 13:03:21 -0400

    Add search bar form

--- frontend/components/search/search_results.jsx
+++ frontend/components/search/search_results.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 class SearchResults extends React.Component {
   render() {
     return(
-      <div className="search-results">
+      <div className="search-results-container">
         search results
       </div>
     )
