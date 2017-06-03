2f4d659f71e2b09f61497f3004cde7e43a261772
Breno_Paiva <breno.paiva93@gmail.com>
2017-06-03 11:49:20 -0400

    Comment out logger

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,16 +1,16 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import createLogger from 'redux-logger';
+// import createLogger from 'redux-logger';
 import RootReducer from '../reducers/root_reducer';
 
-const logger = createLogger();
+// const logger = createLogger();
 
 const middlewares = [thunk];
-
-if (process.env.NODE_ENV === `development`) {
-  const { logger } = require(`redux-logger`);
-    middlewares.push(logger);
-  }
+//
+// if (process.env.NODE_ENV === `development`) {
+//   const { logger } = require(`redux-logger`);
+//     middlewares.push(logger);
+//   }
 
 const configureStore = (preloadedState = {}) => (
   createStore(RootReducer, preloadedState, applyMiddleware(...middlewares))
