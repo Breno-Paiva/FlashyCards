d49bf39cbae6d111fb6b332cf0c275f458049dcf
Breno Paiva <breno.paiva93@gmail.com>
2017-06-07 12:13:21 -0400

    Now uses super awesome and fast search functionality

--- app/controllers/api/search_controller.rb
+++ app/controllers/api/search_controller.rb
@@ -1,9 +1,27 @@
 class Api::SearchController < ApplicationController
 
   def show
-    @subjects = Subject.joins("LEFT OUTER JOIN subscriptions ON subjects.id = subscriptions.subject_id")
-                       .where("LOWER (name) LIKE ?","%#{params[:id]}%")
-                       .includes(decks: :cards)
+    @subjects = ActiveRecord::Base.connection.exec_query(<<-SQL)
+      SELECT
+        subjects.id,
+        subjects.name,
+        max(CASE WHEN (subscriptions.user_id = #{current_user.id}) THEN 1 ELSE 0 END) AS subscribed,
+        COUNT(DISTINCT decks.id) AS deck_count,
+        COUNT(DISTINCT cards.id) AS card_count,
+        COUNT(DISTINCT subscriptions.id) AS learner_count
+      FROM
+        subjects
+      LEFT OUTER JOIN
+        subscriptions ON subjects.id = subscriptions.subject_id
+      LEFT OUTER JOIN
+        decks ON subjects.id = decks.subject_id
+      LEFT OUTER JOIN
+        cards ON decks.id = cards.deck_id
+      WHERE
+        LOWER(subjects.name) LIKE LOWER('%#{params[:id]}%')
+      GROUP BY
+        subjects.id
+    SQL
   end
 
   def search_params
--- app/views/api/search/show.json.jbuilder
+++ app/views/api/search/show.json.jbuilder
@@ -1,22 +1,18 @@
 @subjects.each do |subject|
-  json.set! subject.id do
-    json.extract! subject, :id, :name
-    json.deck_count subject.decks.length
-    # json.deck_count 1
 
-    card_count = 0
-    subject.decks.each do |deck|
-      card_count += deck.cards.length
+  json.set! subject['id'] do
+    json.id subject['id']
+    json.name subject['name']
+    json.deck_count subject['deck_count']
+    json.card_count subject['card_count']
+    json.learner_count subject['learner_count']
+
+    if subject['subscribed'] == 1
+      subscribed = true
+    else
+      subscribed = false
     end
-    json.card_count card_count
 
-    json.learner_count subject.subscriptions.length
-    # json.learner_count 1
-
-    subscribed = false
-    subject.subscriptions.each do |sub|
-      subscribed = true if sub.user_id == current_user.id
-    end
-    json.subscribed  subscribed
+    json.subscribed subscribed
   end
 end
