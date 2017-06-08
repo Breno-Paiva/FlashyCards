d65d3c970848f16ce278924fe8dbb2cc49013977
Breno Paiva <breno.paiva93@gmail.com>
2017-06-08 11:26:27 -0400

    fix subscribe bug on search

--- app/controllers/api/subscriptions_controller.rb
+++ app/controllers/api/subscriptions_controller.rb
@@ -7,14 +7,30 @@ class Api::SubscriptionsController < ApplicationController
   def create
     @subscription = Subscription.new(subscription_params)
     @subscription.user_id = current_user.id
+
     if @subscription.save!
-      # @user = current_user
-      # @subject = Subject.find(subscription_params[:subject_id].to_i)
-      # render "api/subjects/show"
-      subjects = Subject.joins("LEFT OUTER JOIN subscriptions ON subjects.id = subscriptions.subject_id")
-                         .where(id: subscription_params[:subject_id].to_i)
-                         .includes(decks: :cards)
-                         
+       subjects = ActiveRecord::Base.connection.exec_query(<<-SQL)
+         SELECT
+           subjects.id,
+           subjects.name,
+           max(CASE WHEN (subscriptions.user_id = #{current_user.id}) THEN 1 ELSE 0 END) AS subscribed,
+           COUNT(DISTINCT decks.id) AS deck_count,
+           COUNT(DISTINCT cards.id) AS card_count,
+           COUNT(DISTINCT subscriptions.id) AS learner_count
+         FROM
+           subjects
+         LEFT OUTER JOIN
+           subscriptions ON subjects.id = subscriptions.subject_id
+         LEFT OUTER JOIN
+           decks ON subjects.id = decks.subject_id
+         LEFT OUTER JOIN
+           cards ON decks.id = cards.deck_id
+         WHERE
+           subjects.id = '#{subscription_params[:subject_id].to_i}'
+         GROUP BY
+           subjects.id
+      SQL
+
       @subjects = [subjects[0]]
       render "api/search/show"
     else
@@ -24,13 +40,30 @@ class Api::SubscriptionsController < ApplicationController
 
   def destroy
     @subscription = Subscription.find_by subject_id: params[:id], user_id: current_user.id
+
     if @subscription.destroy
-      # @user = current_user
-      # @subject = Subject.find(params[:id].to_i)
-      # render "api/subjects/show"
-      subjects = Subject.joins("LEFT OUTER JOIN subscriptions ON subjects.id = subscriptions.subject_id")
-                         .where(id: params[:id].to_i)
-                         .includes(decks: :cards)
+      subjects = ActiveRecord::Base.connection.exec_query(<<-SQL)
+        SELECT
+          subjects.id,
+          subjects.name,
+          max(CASE WHEN (subscriptions.user_id = #{current_user.id}) THEN 1 ELSE 0 END) AS subscribed,
+          COUNT(DISTINCT decks.id) AS deck_count,
+          COUNT(DISTINCT cards.id) AS card_count,
+          COUNT(DISTINCT subscriptions.id) AS learner_count
+        FROM
+          subjects
+        LEFT OUTER JOIN
+          subscriptions ON subjects.id = subscriptions.subject_id
+        LEFT OUTER JOIN
+          decks ON subjects.id = decks.subject_id
+        LEFT OUTER JOIN
+          cards ON decks.id = cards.deck_id
+        WHERE
+          subjects.id = '#{params[:id].to_i}'
+        GROUP BY
+          subjects.id
+      SQL
+
       @subjects = [subjects[0]]
       render "api/search/show"
     else
